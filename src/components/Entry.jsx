484735b1ff7ba1ac27f53ed5b63cc01c68e498cb
Ali George <george7ali@gmail.com>
2025-01-08 00:27:10 +0100

    create a function in the component and export it

---
+++ src/components/Entry.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Entry(){
+    return (
+        <dl className="dictionary">
+        <div className="term">
+          <dt>
+            <span className="emoji" role="img" aria-label="Tense Biceps">
+              💪
+            </span>
+            <span>Tense Biceps</span>
+          </dt>
+          <dd>
+            “You can do that!” or “I feel strong!” Arm with tense biceps. Also
+            used in connection with doing sports, e.g. at the gym.
+          </dd>
+        </div>
+    </dl>
+    );
+}
+
+export default Entry
